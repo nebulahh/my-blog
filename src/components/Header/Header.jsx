7c20a86ea30fcb0bec3aece0e9ae9b21048bcae3
Lekan <olalekancrown11@gmail.com>
2023-05-30 09:32:07 +1000

    add logout link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import useAuth from '../../hooks/useAuth'
+import useLogout from '../../hooks/useLogout'
 import '../styles/App.css'
 import '../styles/App.min.css'
 
 const Header = () => {
   const { auth } = useAuth()
+  const logout = useLogout()
+  const navigate = useNavigate()
+
+  const signOut = async () => {
+    await logout()
+    navigate('/')
+  }
   return (
     <header className="blog-header py-3 ml-20 mr-20 mb-4">
       <div className="row flex-nowrap justify-content-between align-items-center">
@@ -21,7 +29,14 @@ const Header = () => {
           </nav>
         </div>
         <div className="col-4 d-flex justify-content-end align-items-center">
-          {auth.email && (
+          {auth.email ? (
+            <NavLink
+              onClick={signOut}
+              className="mr-3 bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded-full"
+            >
+              Sign Out
+            </NavLink>
+          ) : (
             <nav>
               <NavLink className="mr-4" to={'/signup'}>
                 Register
